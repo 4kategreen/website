3da033878422e9086a7e216948fa61a0fcb3bfe7
4kategreen <kate@kategreen.us>
2012-10-23 21:05:17 -0400

    Updating resume text.

--- resume.php
+++ resume.php
@@ -1,33 +1,46 @@
   <div id="summary" class="resume_section">
     <h2>Summary</h2>
-    <p class="right">Ten years of experience in web design and development. Have worked all aspects in the development of intelligent, intuitive and adaptive web sites with a focus on delivering a robust and quality product. Has a proven track record of quickly learning and mastering new development methodologies and has successfully implemented them for clients.</p>
+    <p>Over ten years of experience in web design and development. Have worked on all aspects in the development of intelligent, intuitive, and responsive web sites with a focus on delivering a robust and quality product. Has a proven track record of quickly learning and mastering new development methodologies and has successfully implemented them for clients.  </p>
+  </div>
+
+  <div id="skills" class="resume_section">
+    <h2>Skills</h2>
+    <ul>
+      <li>PHP</li>
+      <li>HTML(5)</li>
+      <li>CSS3, LESS</li>
+      <li>jQuery, javascript</li>
+      <li>SQL</li>
+      <li>Git</li>
+      <li>SVN</li>
+      <li>MVC</li>
+      <li>Photoshop</li>
+      <li>Illustrator</li>
+      <li>Apache</li>
+      <li>Drupal</li>
+    </ul>
   </div>
   
   <div id="experience" class="resume_section">
     <h2>Experience</h2>
-    <div class="right">
-    	<div class="company">ITT Exelis, Alexandria, VA <span class="years">1999-present</span></div>
-      <h3>Multiple Positions at the Naval Research Laboratory (NRL), Washington, DC</h3>
-      <div class="position">Web Developer and Designer <span class="years">2008-present</span></div>
-      <p>After joining a large-scale visitor registration project following its first release, quickly learned the application, started to improve upon and document existing code, and created a large component that essentially doubled the application's capabilities by creating a module to register contractors with expired yearly credentials. This worked enabled becoming the lead developer within a year. After the second release, was able to direct the team to manage existing code, start another large code improvement effort, and add another large component that manages all aspects of large scale meetings: meeting creation, security clearances, visitor verification, facility reservations, and attendee check-in.</p>
-      <p>Responsible for the creation of multiple applications at NRL. Supports the entire development process by gathering requirements, researching user needs, creating the design, validating the design with the client, developing the application, testing, and releasing to production. Uses MVC techniques during development while focusing on data integrity, and trying to ensure a positive user experience. Examples of these applications are: a facility meeting scheduler that allows for online reservations, calendar display, and a "facility picker" that helps users narrow down the choice of meeting areas; a publication management system that stores scientist publications to allow for display on websites; and a file repository which allows a scientist to collect data from subjects via automatic and manual file uploads.</p>
-      <p>In addition, to meet a growing need within the group, redesigned templates used for basic websites scientists and researchers use to publicize and offer their publications for sale. These templates were created to support both Drupal and an older, homegrown content management system used only by NRL. The new templates were designed to ensure section 508 compliance, instituted simple, clean page design, and best practice web standards-compliance.</p>
-      <div class="position">Web Designer <span class="years">2005-2008</span></div>
-      <p>Created websites for researchers and scientists at the Naval Research Laboratory using standards-compliant HTML, CSS, and ensured section 508 compliance. Met with and determined the needs of each client, gathered content and publications, edited and transformed content for a less specialized audience, designed or edited graphics and photographs, reviewed the websites with the clients, and created website approval packages as required by the Laboratory.</p>
-      <div class="position">Graphic Designer <span class="years">2005-2008</span></div>
-      <p>In addition to creating websites, created large posters of scientist work from technical papers. Read a paper's content, designed posters and other print displays using Adobe Illustrator, printed on a large plotter, then cut and sometimes framed the documents. These posters were used by scientists for presentations, office and hallway displays, and conferences.</p>
-      <div class="position">Intern, Web Designer, Visual Basic Programmer <span class="years">1999-2005</span></div>
-      <p>Hired as an intern while still at the University of Maryland. Worked in a department support role while finishing college, starting with administrative tasks such as filing, collecting and entering time cards, and running monthly reports. Eventually rose to designing websites and database applications to aid the administrative tasks of my department. Gained a great appreciation for information architecture and user experience, the advantages to proper automation of tasks, and a firm grasp on data integrity through database normalization. Examples of application created are: an employee survey application and a purchase tracking system that generated reconciliations and monthly reports. The purchasing system was used for at least five years.</p>
+    <div class="job">
+      <div class="company">Palladian Partners, Silver Spring, MD</div><div class="years">2012-present</div>
+      <div class="position">Web Developer</div>
+      <p>Designs and builds front end interfaces using LESS, CCS3, jQuery, and HTML. Supports PHP publication repository with testing, user interface improvements, and code refactoring. Supports the team as version control lead and advocates for advanced development practices while still maintaining accessibility and a large static content base.</p>
     </div>
-  </div>
-  
-  <div id="skills" class="resume_section">
-    <h2>Skills</h2>
-    <div class="right">
-      <h3>Programming Languages</h3>
-      <p>PHP, HTML/HTML5, CSS3, LESS, jQuery, SQL (MySQL-focused).</p>
-      <h3>Programs and Applications</h3>
-      <p>Subversion, Photoshop, Illustrator, Drupal.</p>
+    <div class="job">
+    	<div class="company">ITT Exelis, Alexandria, VA</div><div class="years">1999-2012</div>
+      <h3>Multiple Positions at the Naval Research Laboratory (NRL), Washington, DC</h3>
+      <div class="position">Web Developer and Designer</div><div class="years">2008-present</div>
+      <p>After joining a large-scale visitor registration project following its first release, quickly learned the application, started to improve and document existing code. Essentially doubled the application’s capabilities by creating a module to register contractors with expired yearly credentials. This led to becoming the lead developer within a year. Was able to direct the team to manage existing code, start another large code improvement effort, and add another large component that manages all aspects of large scale meetings.</p>
+      <p>Responsible for the creation of multiple applications at NRL. Supported the entire development process by gathering requirements, researching user needs, creating design comps, validating them with the client,  developing the application, testing, and releasing to production. Used MVC techniques during development while focusing on data integrity, and trying to ensure a positive user experience. Examples of these applications are: a facility meeting scheduler that allows for online reservations, and management approval options; a publication management system that stores publications to for website display; and a file repository which collect data from subjects via automatic and manual file uploads.</p>
+      <p>Redesigned templates used for basic websites scientists and researchers use to publicize and offer their publications for sale. These templates were created to support both Drupal and an older, homegrown content management system. The new templates were designed to ensure section 508 compliance, instituted simple, clean page design, and best practice web standards-compliance.</p>
+      <div class="position">Web Designer</div><div class="years">2005-2008</div>
+      <p>Created websites for researchers and scientists at the Naval Research Laboratory using standards-compliant HTML, CSS, and ensured section 508 compliance. Met with and determined the needs of each client, gathered content and publications, edited, and transformed content for a less specialized audience, designed or edited graphics and photographs, reviewed the websites with the clients, and created website approval packages as required by the Laboratory.</p>
+      <div class="position">Graphic Designer</div><div class="years">2005-2008</div>
+      <p>In addition to creating websites, created large posters of scientist work from technical papers. Read a paper’s content, designed posters and other print displays using Adobe Illustrator, printed on a large plotter, then cut and sometimes framed the documents. These posters were used by scientists for presentations, office and hallway displays, and conferences.</p>
+      <div class="position">Intern, Web Designer, Visual Basic Programmer</div><div class="years">1999-2005</div>
+      <p>Hired as an intern while still a student. Worked in a department support role while finishing college, starting with administrative tasks such as filing, collecting and entering time cards, and running monthly reports. Rose to designing websites and database applications to aid the administrative tasks of the department. Gained a great appreciation for information architecture and user experience, the advantages of proper automation of tasks, and a firm grasp on data integrity through database normalization. Examples of application created are: an employee survey application and a purchase tracking system that generated reconciliations and monthly reports.</p>
     </div>
   </div>
   

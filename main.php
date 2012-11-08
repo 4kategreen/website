0dc3d13816b9e0b3de8326535e29a182eb10400a
4kategreen <kate@kategreen.us>
2012-11-08 17:18:48 -0500

    Next step. Adding copy, resumes, images. Still more to do.

--- main.php
+++ main.php
@@ -9,10 +9,17 @@
 </div>
 
 <div id="sidebar">
-	<h2>About</h2>
-	<p>My name is Kate Green. I like solving problems, running, training my dogs, gardening, and just about anything that involves learning something new. My career is web development, where I like to make applications and websites that are easy for people to use and understand. I'm also a RRCA Certified Running Coach.</p>
-	<p>In past lives, I've fostered German Shepherd Dogs and mixes, explored almost every aspect of dog training, got an elementary education degree, and learned and performed almost every wind instrument there is. My focus was trumpet, and I played through college, appearing as "that short trumpet player near the 50 yard line" in the background of various national TV broadcasts.</p>
-	<p>I live with two dogs, Sara and Jack, and one very sassy cat named Pumpkin in the strangely wonderful community of Greenbelt, MD, where not a soul blinks twice at my front yard vegetable garden.</p>
 	<h2>Find Me</h2>
-	<p>Github: <a href="http://github.com/4kategreen">4kategreen</a></p>
+	<ul>
+		<li>Github: <a href="http://github.com/4kategreen">4kategreen</a></li>
+		<li>Fitocracy: <a href="http://www.fitocracy.com/profile/4kategreen">4kategreen</a>
+	</ul>
+
+	<h2>About</h2>
+	<div id="about">
+		<p class="kate">My name is Kate Green. I like solving problems, running, training my dogs, gardening, and just about anything that involves learning something new. My career is web development, where I like to make applications and websites that are easy for people to use and understand. I'm also a RRCA Certified Running Coach.</p>
+		<p>In past lives, I've fostered German Shepherd Dogs and mixes, explored almost every aspect of dog training, got an elementary education degree, and learned and performed almost every wind instrument there is. My focus was trumpet, and I played through college, appearing as "that short trumpet player near the 50 yard line" in the background of various national TV broadcasts.</p>
+		<p>I live with two dogs, Sara and Jack, and one very sassy cat named Pumpkin in the strangely wonderful community of Greenbelt, MD, where not a soul blinks twice at my front yard vegetable garden.</p>
+		<div class="pets"></div>
+	</div>
 </div>

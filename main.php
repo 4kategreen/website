425c16e6b02023c804634428c48043ec1b30a792
4kategreen <kate@kategreen.us>
2012-12-18 21:29:32 -0500

    I'm not looking for a job anymore.

--- main.php
+++ main.php
@@ -1,5 +1,4 @@
 <div id="body">
-	<p>Kate Green is seeking a full time web application developer position in the Washington, DC area. To learn more, please visit my resume or download it here.</p>
 
 	<h2>Take my Resume With You</h2>	
 	<ul>

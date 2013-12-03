521602d21b6fcdbd69d0431944a858494558d8e8
4kategreen <kate@kategreen.us>
2013-12-03 14:41:13 -0500

    app name change.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('websiteApp')
+angular.module('kategreen')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- app/scripts/controllers/resume.js
+++ app/scripts/controllers/resume.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('websiteApp')
+angular.module('kategreen')
   .controller('ResumeCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',

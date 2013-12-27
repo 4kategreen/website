8bdf9c46d860e3b8fc66dd18019e59ac351eb5c7
4kategreen <kate@kategreen.us>
2013-12-27 16:47:17 -0500

    removing sample tests.

--- test/spec/controllers/blog.js
+++ test/spec/controllers/blog.js
@@ -15,8 +15,5 @@ describe('Controller: BlogCtrl', function () {
       $scope: scope
     });
   }));
-
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  
 });
--- test/spec/controllers/resume.js
+++ test/spec/controllers/resume.js
@@ -16,7 +16,7 @@ describe('Controller: ResumeCtrl', function () {
     });
   }));
 
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  xit('should get the resume data and display it.', function() {
+
   });
 });

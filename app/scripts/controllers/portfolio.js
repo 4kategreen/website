c34c7aa437e74e738b51d847b1c2b23804c5a4da
Kate Green <kate.green@chronicle.com>
2014-02-12 13:12:59 -0500

    Adding some portfolio dummy data and the start of some portfolio tiles. Need images.

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -2,9 +2,102 @@
 
 angular.module('kategreenApp')
   .controller('PortfolioCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.portfolioItems = [
+  {
+    "name": "convallis dolor quisque tincidunt pede ac",
+    "type": "print",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed",
+    "date": "1410957225"
+  },
+  {
+    "name": "adipiscing ligula aenean",
+    "type": "shirt",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at",
+    "date": "1365520045"
+  },
+  {
+    "name": "Etiam imperdiet dictum magna ut",
+    "type": "print",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus",
+    "date": "1415810600"
+  },
+  {
+    "name": "Phasellus elit pede,",
+    "type": "shirt",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut",
+    "date": "1379229512"
+  },
+  {
+    "name": "congue in scelerisque",
+    "type": "code",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et",
+    "date": "1367469908"
+  },
+  {
+    "name": "eu",
+    "type": "print",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.",
+    "date": "1376219433"
+  },
+  {
+    "name": "massa lobortis ultrices vivamus",
+    "type": "web",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien,",
+    "date": "1423029257"
+  },
+  {
+    "name": "egestas a, dui cras pellentesque.",
+    "type": "code",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus",
+    "date": "1413306239"
+  },
+  {
+    "name": "pede cras vulputate velit eu sem.",
+    "type": "code",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida",
+    "date": "1376967696"
+  },
+  {
+    "name": "Aenean sed pede nec",
+    "type": "code",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed",
+    "date": "1384688442"
+  },
+  {
+    "name": "accumsan neque et nunc",
+    "type": "web",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec",
+    "date": "1403366879"
+  },
+  {
+    "name": "urna suscipit nonummy fusce fermentum fermentum",
+    "type": "web",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et",
+    "date": "1395398674"
+  },
+  {
+    "name": "placerat velit quisque varius nam",
+    "type": "print",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer",
+    "date": "1386576610"
+  },
+  {
+    "name": "enim suspendisse",
+    "type": "web",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida",
+    "date": "1365768990"
+  },
+  {
+    "name": "per conubia nostra, per inceptos hymenaeos",
+    "type": "shirt",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed",
+    "date": "1383172623"
+  },
+  {
+    "name": "per conubia nostra per",
+    "type": "code",
+    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus.",
+    "date": "1403648348"
+  }
+];
   });
